b0119886af912bdb93965ef7a4b0a9e35dfa61fe
Priyanshu Gupta <priyanshu2859@gmail.com>
2023-09-10 01:40:54 +0530

    header added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,30 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
-
-const inter = Inter({ subsets: ['latin'] })
+import { PostCard,Categories,PostWidget } from '@/components'
+const posts=[
+  {title:'Aur sunao',excerpt:'Baat bahi kya chal raaha hai'},
+  {title:'Hey',excerpt:'Hey whatsapp, everything fine'},
+]
 
 export default function Home() {
   return (
     <div className='container mx-auto px-10 mb-8'>
       <Head>
-        <title>Kamlesh Ki Katha</title>
+        <title>Babloo Kay Blog</title>
         <link rel='icon' href='/favicon.ico'></link>
       </Head>
+      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+        <div className='lg:col-span-8 col-span-1'>
+        {posts.map((post,index)=>(<PostCard post={post} key={index} />))}
+        </div>
+        <div className='lg:col-span-4 col-span-1'>
+          <div className='lg:sticky relative top-8'>
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

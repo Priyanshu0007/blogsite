e7876d0adda00b107bf17bef44d791eafc7d1e20
Priyanshu Gupta <priyanshu2859@gmail.com>
2023-09-10 02:18:55 +0530

    graphQl fetching

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,9 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import Head from 'next/head'
 import { PostCard,Categories,PostWidget } from '@/components'
-const posts=[
-  {title:'Aur sunao',excerpt:'Baat bahi kya chal raaha hai'},
-  {title:'Hey',excerpt:'Hey whatsapp, everything fine'},
-]
+import { getPosts } from '../services'
 
-export default function Home() {
+
+export default function Home({posts}) {
   return (
     <div className='container mx-auto px-10 mb-8'>
       <Head>
@@ -16,7 +12,7 @@ export default function Home() {
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-        {posts.map((post,index)=>(<PostCard post={post} key={index} />))}
+          {posts.map((post,index)=>(<PostCard post={post} key={index} />))}
         </div>
         <div className='lg:col-span-4 col-span-1'>
           <div className='lg:sticky relative top-8'>
@@ -27,4 +23,11 @@ export default function Home() {
       </div>
     </div>
   )
+}
+
+export async function getStaticProps(){
+  const posts=(await getPosts())|| [];
+  return {
+    props:{posts}
+  }
 }

37f37dd026f344346549cab268c4f67ede5131b8
Priyanshu Gupta <priyanshu2859@gmail.com>
2023-09-10 03:12:39 +0530

    post added

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default function Home({posts}) {
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post,index)=>(<PostCard post={post} key={index} />))}
+          {posts.map((post,index)=>(<PostCard post={post.node} key={index} />))}
         </div>
         <div className='lg:col-span-4 col-span-1'>
           <div className='lg:sticky relative top-8'>
@@ -27,7 +27,5 @@ export default function Home({posts}) {
 
 export async function getStaticProps(){
   const posts=(await getPosts())|| [];
-  return {
-    props:{posts}
-  }
+  return {props:{posts}}
 }

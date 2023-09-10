3823ab4217987eb625248f6ef808471d23285702
Priyanshu Gupta <priyanshu2859@gmail.com>
2023-09-10 19:28:08 +0530

    comments added

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import moment from 'moment';
+import parse from 'html-react-parser';
 
-const Comments = () => {
+import { getComments } from '../services';
+
+const Comments = ({ slug }) => {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    getComments(slug).then((result) => {
+      setComments(result);
+    });
+  }, []);
+
   return (
-    <div>Comments</div>
-  )
-}
+    <>
+      {comments.length > 0 && (
+        <div className=" shadow-lg rounded-lg p-8 pb-12 mb-8 bg-gray-300  bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40">
+          <h3 className="text-xl mb-8 text-slate-700 font-semibold border-gray-500 border-b pb-4">
+            {comments.length}
+            {' '}
+            Comments
+          </h3>
+            {comments.map((comment, index) => (
+              <div key={index} className="border-b border-gray-500 mb-4 pb-4">
+                <p className="mb-4">
+                  <span className="font-semibold text-slate-800">{comment.name}</span>
+                  {' '}
+                  on
+                  {' '}
+                  {moment(comment.createdAt).format('MMM DD, YYYY')}
+                </p>
+                <p className="whitespace-pre-line text-gray-800 w-full">{parse(comment.comment)}</p>
+              </div>
+            ))}
+        </div>
+      )}
+    </>
+  );
+};
 
-export default Comments
+export default Comments;

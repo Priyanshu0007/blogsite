d4ca8529e63c84611341012973e1fea56f46d906
Priyanshu Gupta <priyanshu2859@gmail.com>
2023-09-10 22:07:13 +0530

    bug fixing 1

--- src/pages/category/[slug].js
+++ src/pages/category/[slug].js
@@ -3,19 +3,23 @@ import { useRouter } from 'next/router';
 
 import { getCategories, getCategoryPost } from '../../services';
 import { PostCard, Categories, Loader } from '../../components';
+import Head from 'next/head';
 
 const CategoryPost = ({ posts }) => {
   const router = useRouter();
-
   if (router.isFallback) {
     return <Loader />;
   }
 
   return (
     <div className="container mx-auto px-10 mb-8">
+      <Head>
+          <title>{posts[0].node.categories[0].name}</title>
+          <link rel='icon' href='/favicon.ico'></link>
+      </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8">
-          {posts.map((post, index) => (
+          {posts.reverse().map((post, index) => (
             <PostCard key={index} post={post.node} />
           ))}
         </div>

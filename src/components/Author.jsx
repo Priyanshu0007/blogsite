04be1fc43fd1f2d86648b8f0a21f718d37c01d9d
Priyanshu Gupta <priyanshu2859@gmail.com>
2023-09-10 16:14:08 +0530

    author component added

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
-const Author = () => {
+import Image from 'next/image'
+const Author = ({author}) => {
   return (
-    <h1>Author</h1>
+    <div className='text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20'>
+      <div className='absoulte left-0 right-0 -top-14'>
+      <Image
+        unoptimized
+        alt={author.name}
+        height="100"
+        width="100"
+        className="align-middle rounded-full "
+        src={author.photo.url}/>
+        <h3 className='text-white my-4 text-xl font-bold'>{author.name}</h3>
+        <p className='text-white text-lg'>{author.bio}</p>
+      </div>
+    </div>
   )
 }
 
